d0542226e85a900545fa67546e94fec8b751af58
Paolo Ventura <ventura.paolo@gmail.com>
2018-10-26 14:32:52 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,7 +2,8 @@
 ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(sentences_array)
-  sentences_array.sort_by { |sentence| sentence.split("").map 
-      { |character| ESPERANTO_ALPHABET.index(character)}
-  }
+  sentences_array.sort_by do |sentence| sentence.split("").map 
+      do |character| ESPERANTO_ALPHABET.index(character)
+      end
+  end
 end
