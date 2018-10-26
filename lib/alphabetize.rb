b7cadb8ca85d3b4202e89f0903065d7b70afb1b4
Paolo Ventura <ventura.paolo@gmail.com>
2018-10-26 14:25:57 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,7 @@
-def alphabetize(arr)
-
-  a = arr.sort
-  
-  
+def alphabetize(sentences_array)
+  sentences_array.sort_by do |sentence|
+    sentence.split("").map do |character|
+      ESPERANTO_ALPHABET.index(character)
+    end
+  end
 end
